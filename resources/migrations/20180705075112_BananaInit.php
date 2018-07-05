6d2e9fd89b4c480acbe83578112e4ec0247f05c7
yinminc <a1598742002@gmail.com>
2018-07-05 15:52:48 +0800

    利用 terminal 輸入指令新增一個資料表： php windwalker migration create BananaInit

---
+++ resources/migrations/20180705075112_BananaInit.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) {YEAR} LYRASOFT. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+use Windwalker\Core\Migration\AbstractMigration;
+use Windwalker\Database\Schema\Column;
+use Windwalker\Database\Schema\DataType;
+use Windwalker\Database\Schema\Schema;
+
+/**
+ * Migration class, version: 20180705075112
+ */
+class BananaInit extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
